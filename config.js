029362a6261d6d7cfb7364dea7edb0c8c1108933
kelsthevibe <94661041+kelsthevibe@users.noreply.github.com>
2022-09-09 17:17:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
+global.Owner = ['2348181117629']
+global.OwnerNumber = ['2348181117629']
+global.ownertag = ['2348181117629']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
